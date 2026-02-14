91f11d7031418958b4ac5d433397e3ab8685a89c
sylvainmahetest <sylvainmahe4815@gmail.com>
2026-02-14 03:45:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1156,7 +1156,7 @@ function particuleAnimation()
             //colorAlpha[indexGreen] = 1 - (0.2 * (1 - magnitude));
             
             colorAlpha[indexRed] = 1 - (0.25 * magnitude);
-            colorAlpha[indexGreen] = (1 - (0.75 * (1 - magnitude))) * fadeIn;
+           // colorAlpha[indexGreen] = (1 - (0.75 * (1 - magnitude))) * fadeIn;
             colorAlpha[indexAlpha] = (alphaStart[indexParticule] - (alphaStart[indexParticule] * 0.5 * magnitude)) * fadeIn;
             //////////////////////////(0.1 - (0.1 * 0.5 * 1))
         }
